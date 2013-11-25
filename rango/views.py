f7ef27b1d378bde1a460642b7cd9b82ba0a43da2
Paul Nichols <pnichols104@gmail.com>
2013-11-25 07:58:25 -0500

    add_page view not displaying

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,11 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category, Page
+from forms import CategoryForm, PageForm
+
+
+def decode_url(url):
+	return url.replace('_', ' ')
 
 def index(request):
 	context = RequestContext(request)
@@ -19,8 +24,9 @@ def about(request):
 def category(request, category_name_url):
 	context = RequestContext(request)
 
-	category_name = category_name_url.replace('_', ' ')
-	context_dict = {'category_name':category_name}
+	category_name = decode_url(category_name_url)
+	context_dict = {'category_name':category_name, 
+					'category_name_url':category_name_url}
 	try:
 		category = Category.objects.get(name=category_name)
 		pages = Page.objects.filter(category=category)
@@ -30,4 +36,52 @@ def category(request, category_name_url):
 		pass
 	return render_to_response('rango/category.html', context_dict, context)
 
+def add_category(request):
+	context = RequestContext(request)
 
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print form.errors
+
+	else:
+		form = CategoryForm()
+
+	return render_to_response('rango/add_category.html', {'form':form}, context)
+
+
+def add_page(request, category_name_url):
+	print 'add_page called'
+	context = RequestContext(request)
+
+	category_name = decode_url(category_name_url)
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+
+		if form.is_valid():
+			page = form.save(commit = False)
+
+			cat = Category.objects.get(name = category_name)
+			page.category = cat
+
+			page.views = 0
+
+			page.save()
+
+			return category(request, category_name_url)
+
+		else:
+			print form.errors
+
+	else:
+		form = PageForm()
+
+	return render_to_response('rango/add_page.html',
+		{'category_name_url': category_name_url,
+		'category_name': category_name, 'form':form},
+		context)
