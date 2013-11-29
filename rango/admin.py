12058fda0a0423d30e6f6cec6c7a9c2cf9026472
Paul Nichols <pnichols104@gmail.com>
2013-11-29 11:31:10 -0500

    up to 8.6

--- rango/admin.py
+++ rango/admin.py
@@ -5,3 +5,4 @@ class PageAdmin(admin.ModelAdmin):
 	list_display = ('category', 'title', 'url')
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Category(models.Model):
 	name 	= models.CharField(max_length=128, unique=True)
 	views 	= models.IntegerField(default=0)
@@ -20,3 +20,10 @@ class Page(models.Model):
 	def __unicode__(self):
 		return self.title
 
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	def __unicode__(self):
+		return self.user.username
