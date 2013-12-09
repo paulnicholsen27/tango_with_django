1784bc6e6542c7b592d5fe69ed65c3d76023c51c
Paul Nichols <pnichols104@gmail.com>
2013-12-09 07:52:44 -0500

    finished 12.2

---
+++ rango/bing_search.py
@@ -0,0 +1,50 @@
+import json
+import urllib, urllib2
+
+def run_query(q):
+	root_url = 'https://api.datamarket.azure.com/Bing/Search/'
+	source = 'Web'
+
+	results_per_page = 10
+	offset = 0
+
+	query = "'{0}'".format(q)
+	query = urllib.quote(query)
+
+	search_url = "{0}{1}?$format=json$$top={2}&$skip={3}&Query={4}".format(
+		root_url,
+		source,
+		results_per_page,
+		offset,
+		query)
+
+	username = ''
+	bing_api_key = 't6LdLN1d0Ma02pTlLVnuLklokO5JiPVl84Q+en7aPoE'
+
+	password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+	password_mgr.add_password(None, search_url, username, bing_api_key)
+
+	results = []
+
+	try:
+		handler = urllib2.HttpBasicAuthHandler(password_mgr)
+		opener = urllib2.build_opener(handler)
+		urllib2.install_opener(opener)
+
+		response = urllib2.urlopen(search_url).read()
+
+		json_response = json.loads(response)
+
+		for result in json_response['d']['results']:
+			results.append({
+				'title':result['Title'],
+				'link':result['Url'],
+				'summary':result['Description']
+				})
+
+	except urllib2.URLError, e:
+		print "Error when querying the Bing API: ", e
+
+	return results
+
+	
